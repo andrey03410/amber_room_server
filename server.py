c29aacc5985a059fc702bf2f7cd30a3ab30e0103
andrey03410 <Andrey.gel001234@mail.ru>
2023-03-19 02:06:16 +0200

    fix get

--- server.py
+++ server.py
@@ -8,16 +8,22 @@ app = Flask(__name__)
 def get_persons():
     persons = [{'id': 1, 'name': 'test1', 'description': 'testdesc1', 'id_nationality': 1},
                {'id': 2, 'name': 'test1', 'description': 'testdesc1', 'id_nationality': 1}]
-    response = Response(response=json.dumps({'persons': persons}), status=200, headers=None,
-                        content_type='application/json')
+    # response = Response(response=json.dumps({'persons': persons}), status=200, headers=None,
+    #                     content_type='application/json')
+    # response.headers['Access-Control-Allow-Origin'] = '*'
+    response = jsonify({'persons': persons})
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.content_type = 'application/json'
+    response.status = 200
     return response
 
 
-@app.route('/addPerson', methods=['POST'])
+@app.route('/addPerso')
 def add_person():
-    print("Персона добавлена")
-    return "test"
+    response = Response(status=200)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.content_type = 'application/json'
+    return response
 
 
 if __name__ == '__main__':
